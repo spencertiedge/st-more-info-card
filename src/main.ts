fe80939a0999dd1b9f217fa08a79141da29160ad
spencertiedge <75261143+spencertiedge@users.noreply.github.com>
2024-09-16 12:09:34 -0500

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -80,10 +80,6 @@ class MoreInfoCard extends LitElement {
                 ${DOMAINS_NO_INFO.includes(domain)
                   ? ""
                   : html`
-                      <state-card-content
-                        .stateObj=${stateObj}
-                        .hass=${this.hass}
-                      ></state-card-content>
                     `}
                 <more-info-content
                   .hass=${this.hass}
